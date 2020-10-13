70496de9462320470a59651134e1938304fe3508
Sponya <viteksilin@mail.ru>
2020-10-13 22:13:45 +0300

    First code

---
+++ lab3/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+
+namespace Lab3
+{
+
+    public partial class Viewer
+    {
+
+    }
+
+    public class Program
+    {
+        static bool check = false;
+        static Car[] mas2 = null;
+
+        public static void Main()
+        {
+            string a;
+            while(true)
+            {
+                while(true)
+                {
+                    WriteLine("Выберите нужный пункт меню:");
+                    WriteLine("1 - создать машины:");
+                    WriteLine("2 - вывести машины(WriteLine):");
+                    WriteLine("3 - вывести список машин по марке:");
+                    WriteLine("4 - вывести список машин, которые используются больше заданного срока:");
+                    WriteLine("5 - очистить консоль:");
+                    WriteLine("6 - конструкторы:");
+                    WriteLine("7 - вывод возраста выбранной машины:");
+                    WriteLine("8 - вывод информации о классе:");
+                    WriteLine("9 - :пример работы с out");
+                    WriteLine("10 - вывод машин(ToString):");
+                    WriteLine("11 - анонимный тип:");
+                    WriteLine("12 - вызов закрытого конструктора:");
+                    WriteLine("0 - для выхода:");
+                    a = ReadLine();
+                    if (a == "1")
+                    {
+                        check = true;
+                        break;
+                    }
+                    if (a == "2" && check == false || a == "3" && check == false || a == "4" && check == false || a == "7" && check == false || a == "10" && check == false)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"////////////////////////////////////////////////////");
+                        Console.WriteLine("Автобусов в базе данных нет!");
+                        Console.WriteLine($"////////////////////////////////////////////////////");
+                    }
+                    else { break; }
+                }
+                switch (a)
+                {
+                    case "1":
+                        mas2 = Car.CreateACar(ref check);
+                        continue;
+                    case "2":
+                        Car.Output(ref mas2);
+                        continue;
+                    case "3":
+                        Car.ListCar(ref mas2);
+                        continue;
+                    case "4":
+                        Car.ListCarAge(ref mas2);
+                        continue;
+                    case "5":
+                        Clear();
+                        continue;
+                    case "6":
+                        Construct();
+                        continue;
+                    case "7":
+                        Car.CarAges(mas2);
+                        continue;
+                    case "8":
+                        Car.Info();
+                        continue;
+                    case "9":
+                        int numb = 0;
+                        Car.Prm(out numb);
+                        WriteLine(numb);
+                        continue;
+                    case "10":
+                        Car.OutputWitchToString(mas2);
+                        continue;
+                    case "11":
+                        Anonimous();
+                        continue;
+                    case "12":
+                        UltraCar.OutputPrivate();
+                        continue;
+                    case "0": break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Ошибка ввода, такого пункта в меню нет");
+                        continue;
+                }
+                break;
+            }
+        }
+        static void Construct() //пример конструкторов
+        {
+            Car car1 = new Car("PA2948J79", "Mazda", "6", 23000, "красный", 2015, "9153UN03");
+            Car car2 = new Car();
+            WriteLine($"Машина car1");
+            WriteLine($"ID машины: {car1.CarID}");
+            WriteLine($"Марка машины: {car1.CarMark}");
+            WriteLine($"Модель машины: {car1.CarModel}");
+            WriteLine($"Цена машины в $: {car1.CarPrice}");
+            WriteLine($"Цвет машины: {car1.CarColor}");
+            WriteLine($"Год начала эксплуатации: {car1.CarAge}");
+            WriteLine($"Регистрационный номер машины: {car1.CarNumb}");
+            WriteLine($"////////////////////////////////////////////////////");
+            WriteLine($"Машина car2");
+            WriteLine($"ID машины: {car2.CarID}");
+            WriteLine($"Марка машины: {car2.CarMark}");
+            WriteLine($"Модель машины: {car2.CarModel}");
+            WriteLine($"Цена машины: {car2.CarPrice}");
+            WriteLine($"Цвет машины: {car2.CarColor}");
+            WriteLine($"Год начала эксплуатации: {car2.CarAge}");
+            WriteLine($"Регистрационный номер машины: {car2.CarNumb}");
+            WriteLine($"////////////////////////////////////////////////////");
+        }
+        static void Anonimous()
+        {
+            var anonimous = new { firstname = "V", age = 777, secondname = " - Vendetta" };
+            WriteLine($"////////////////////////////////////////////////////");
+            WriteLine($"Данный тип: {anonimous.GetType()}");
+            WriteLine($"////////////////////////////////////////////////////");
+        }
+
+    }
+}
---
+++ lab3/Stack.cs
@@ -0,0 +1,477 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+using Microsoft.VisualBasic;
+using System.Net.NetworkInformation;
+
+namespace Lab3
+{
+    public class Car
+    {
+        string car_id;
+        string car_mark;
+        string car_model;
+        uint car_age;
+        string car_color;
+        uint car_price;
+        string car_numb;
+        public static int CountOfCar = 0;
+        public readonly string read = " Только что чтения ";
+
+
+        public string CarID
+        {
+            get
+            {
+                return car_id;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(" ID не может быть пустым");
+                }
+                car_id = value;
+            }
+        }
+
+        public string CarMark
+        {
+            get
+            {
+                return car_mark;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(" Марка машины не может быть пустой");
+                }
+                car_mark = value;
+            }
+        }
+
+        public string CarModel
+        {
+            get
+            {
+                return car_model;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(" Модель машины не может быть пустой");
+                }
+                car_model = value;
+            }
+        }
+
+        public uint CarAge
+        {
+            get
+            {
+                return car_age;
+            }
+            set
+            {
+                if (value > 2020)
+                {
+                    while (value > 2020)
+                    {
+
+                        Console.WriteLine("Год начала эксплуатации введён некорректно, введите год начала эксплуатации: ");
+
+                        value = Convert.ToUInt32(Console.ReadLine());
+                    }
+                }
+                car_age = value;
+            }
+        }
+
+        public string CarColor
+        {
+            get
+            {
+                return car_color;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(" Цвет машины не может быть пустым");
+                }
+                car_color = value;
+            }
+        }
+
+        public uint CarPrice
+        {
+            get
+            {
+                return car_price;
+            }
+            set
+            {
+                car_price = value;
+            }
+        }
+
+        public string CarNumb
+        {
+            get
+            {
+                return car_numb;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(" Регистрационный номер не может быть пустым");
+                }
+                car_numb = value;
+            }
+        }
+
+        public Car(string ci, string cma, string cmo, uint ca, string cc, uint cp, string cn)
+        {
+            car_id = ci;
+            car_mark = cma;
+            car_model = cmo;
+            car_age = ca;
+            car_color = cc;
+            car_price = cp;
+            car_numb = cn;
+        }
+
+        public Car()
+        {
+            car_id = "AA22O8K13";
+            car_mark = "BMW";
+            car_model = "X8";
+            car_age = 2016;
+            car_color = " Индиго";
+            car_price = 25000;
+            car_numb = "5281OP07";
+        }
+
+        static Car()
+        {
+            WriteLine("Вызов статического конструктора\n\n");
+
+        }
+
+        public static void Info()
+        {
+            WriteLine($"////////////////////////////////////////////////////");
+            WriteLine($"Информация о классе . . .  Он в себе содержит:");
+            WriteLine($"ID машины");
+            WriteLine($"Марка машины");
+            WriteLine($"Модель машины");
+            WriteLine($"Год начала эксплуатации");
+            WriteLine($"Цвет машины");
+            WriteLine($"Цена машины");
+            WriteLine($"Регистрационный номер");
+            WriteLine($"////////////////////////////////////////////////////");
+
+        }
+
+        public static void CarAges(Car[] mas)
+        {
+            while (true)
+            {
+                byte age;
+                while (true)
+                {
+                    WriteLine($"////////////////////////////////////////////////////");
+                    WriteLine($"Введите номер машины( 0 - для возвращения обратно): ");
+                    WriteLine($"////////////////////////////////////////////////////");
+                    string a = ReadLine();
+
+                    if (a == "0")
+                    {
+                        Program.Main();
+                    }
+                    try
+                    {
+                        age = Convert.ToByte(a);
+                        break;
+                    }
+                    catch (SystemException)
+                    {
+                        WriteLine($"////////////////////////////////////////////////////");
+                        WriteLine($"Номер введен некорректно");
+                        WriteLine($"////////////////////////////////////////////////////");
+                        continue;
+                    }
+                }
+                if (age > Convert.ToByte(mas.Length) || age < 0)
+                {
+                    WriteLine($"////////////////////////////////////////////////////");
+                    WriteLine($"Такого номера машины нет");
+                    WriteLine($"////////////////////////////////////////////////////");
+                    continue;
+                }
+                else
+                {
+                    WriteLine($"////////////////////////////////////////////////////");
+                    WriteLine($" Возраст машины номер {age}: {2020 - mas[age - 1].car_age}");
+                    break;
+                }
+            }
+        }
+
+
+        public static Car[] CreateACar(ref bool check)
+        {
+            string i;
+            int count;
+            while (true)
+            {
+                while (true)
+                {
+                    WriteLine($"////////////////////////////////////////////////////");
+                    WriteLine("Введите количество машин (0 для отмены):");
+                    WriteLine($"////////////////////////////////////////////////////");
+                    i = ReadLine();
+                    try
+                    {
+                        count = Convert.ToInt32(i);
+                        break;
+                    }
+                    catch (SystemException)
+                    {
+                        WriteLine($"////////////////////////////////////////////////////");
+                        WriteLine("Некорректно введено машин");
+                        WriteLine($"////////////////////////////////////////////////////");
+                        continue;
+                    }
+                }
+                if (count < 0)
+                {
+                    WriteLine($"////////////////////////////////////////////////////");
+                    WriteLine("Некорректно введено кол-во машин");
+                    WriteLine($"////////////////////////////////////////////////////");
+                    continue;
+                }
+                if (count == 0)
+                {
+                    check = false;
+                    break;
+                }
+                else break;
+            }
+
+            Car[] mas = new Car[count];
+            for (int u = 0; u < count; u++)
+            {
+                mas[u] = new Car();
+                WriteLine($"////////////////////////////////////////////////////");
+                WriteLine($"Машина номер {i + 1}");
+                WriteLine("Введите ID автомобиля:");
+                mas[u].CarID = ReadLine();
+                WriteLine("Введите регистрационный номер машины(8 знаков)");
+                mas[u].CarNumb = ReadLine();
+                if (mas[u].CarNumb.Length != 8)
+                {
+                    while (mas[u].CarNumb.Length != 8)
+                    {
+                        WriteLine($"////////////////////////////////////////////////////");
+                        WriteLine("Некорректно введён номер машины");
+                        WriteLine("Введите регистрационный номер машины(8 знаков):");
+                        mas[u].CarNumb = ReadLine();
+
+                    }
+                }
+                while (true)
+                {
+                    WriteLine("Введите цену автомобиля в $:");
+                    i = ReadLine();
+                    try
+                    {
+                        mas[u].CarPrice = Convert.ToUInt32(i);
+                        break;
+                    }
+                    catch (SystemException)
+                    {
+                        WriteLine($"////////////////////////////////////////////////////");
+                        Console.WriteLine("Цена введена неверно");
+                        WriteLine($"////////////////////////////////////////////////////");
+                    }
+                }
+                WriteLine("Введите марку автомобиля:");
+                mas[u].CarMark = ReadLine();
+                WriteLine("Введите модель автомобиля:");
+                mas[u].CarModel = ReadLine();
+                WriteLine("Введите цвет автомобиля:");
+                mas[u].CarColor = ReadLine();
+                while (true)
+                {
+                    WriteLine("Введите год начала эксплуатации(4 числа):");
+                    i = ReadLine();
+                    if (i.Length != 4)
+                    {
+                        WriteLine($"////////////////////////////////////////////////////");
+                        WriteLine("Год начала эксплуатации введён неверно");
+                        WriteLine($"////////////////////////////////////////////////////");
+                        continue;
+                    }
+                    try
+                    {
+                        mas[u].CarAge = Convert.ToUInt32(i);
+                        break;
+                    }
+                    catch (SystemException)
+                    {
+                        WriteLine($"////////////////////////////////////////////////////");
+                        WriteLine("Год начала эксплуатации введён неверно");
+                        WriteLine($"////////////////////////////////////////////////////");
+                    }
+                }
+            }
+            Console.Clear();
+            return mas;
+        
+        }
+
+         public static void Output(ref Car[] mas) //Вывод всех экземпляров из массива
+        {
+            for (int i = 0; mas.Length > i; i++)
+            {
+                Console.WriteLine($"////////////////////////////////////////////////////");
+                Console.WriteLine($"Машина номер {i + 1}");
+                Console.WriteLine($"ID машины: {mas[i].CarID}");
+                Console.WriteLine($"Марка машины: {mas[i].CarMark}");
+                Console.WriteLine($"Модель машины: {mas[i].CarModel}");
+                Console.WriteLine($"Цвет машины: {mas[i].CarColor}");
+                Console.WriteLine($"Цена машины: {mas[i].CarPrice}");
+                Console.WriteLine($"Год начала эксплуатации: {mas[i].CarAge}");
+                Console.WriteLine($"Регистрационный номер машины: {mas[i].CarNumb}");
+                Console.WriteLine($"////////////////////////////////////////////////////");
+            }
+            Car.CountOfCar = mas.Length;
+            Console.WriteLine($"Всего Машин:{Car.CountOfCar}");
+            Console.WriteLine($"////////////////////////////////////////////////////");
+        }
+
+        public static void ListCar(ref Car[] mas)
+        {
+            string b;
+            byte count = 0;
+            while (true)
+            {
+                WriteLine("Введите марку автомобиля(-ей), которые вы хотите посмотреть");
+                string a = ReadLine();
+                try
+                {
+                    b = Convert.ToString(a);
+                    break;
+                }
+                catch (SystemException)
+                {
+                    WriteLine($"////////////////////////////////////////////////////");
+                    WriteLine("Значение введено некорректно");
+                    WriteLine($"////////////////////////////////////////////////////");
+                }
+            }
+            WriteLine($"////////////////////////////////////////////////////");
+            for (int i = 0; mas.Length > i; i++)
+            {
+                if (mas[i].CarMark == b)
+                {
+                    WriteLine($"Автомобиль номер {i + 1} марки {b}");
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                WriteLine($"Автобусов с такой маркой нет :( ");
+            }
+            WriteLine($"////////////////////////////////////////////////////");
+        }
+
+        public static void ListCarAge(ref Car[] mas)
+        {
+            uint b;
+            byte count = 0;
+            while (true)
+            {
+                WriteLine($"Введите количество лет. Автомобили, которые используются больше заданного срока будут выведены на ваше обозрение  ");
+                string a = ReadLine();
+                if (Convert.ToInt32(a) > 100 || Convert.ToInt32(a) < 0)
+                {
+                    WriteLine($"Некорректное количество лет");
+                    continue;
+                }
+                try
+                {
+                    b = Convert.ToUInt32(a);
+                    break;
+                }
+                catch (SystemException)
+                {
+                    WriteLine($"Некорректное количество лет");
+                    continue;
+                }
+            }
+            WriteLine($"////////////////////////////////////////////////////");
+            for ( int i = 0; mas.Length > i; i++)
+            {
+                int age = 2020;
+                if ( b < (age - mas[i].CarAge))
+                {
+                    WriteLine($"Автомобиль номер {i + 1}");
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                WriteLine("Подходящих машин нет");
+            }
+            WriteLine($"////////////////////////////////////////////////////");
+        }
+
+        public static void Prm(out int num)
+        {
+            num = 111 * 7;
+        }
+
+        public static void OutputWitchToString(Car[] mas)
+        {
+            for ( int i = 0; mas.Length > i; i++)
+            {
+                WriteLine(mas[i].ToString());
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{car_id} {car_mark} {car_model} {car_age} {car_color} {car_price} {car_numb}";
+        }
+
+        public override int GetHashCode()
+        {
+            return car_id.GetHashCode();
+        }
+    }
+
+    public class UltraCar : Car
+    {
+
+        private UltraCar()
+        {
+            WriteLine($"////////////////////////////////////////////////////");
+            WriteLine("Москвич Ребеша Марка");
+            WriteLine($"////////////////////////////////////////////////////");
+        }
+
+        public static void OutputPrivate()
+        {
+            UltraCar uc = new UltraCar();
+        }
+    }
+    public partial class Viewer
+    {
+
+    }
+}
